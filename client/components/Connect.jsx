51b9184e8dafae8cd158e86f6ac448993a5b6847
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2020-06-23 09:55:52 -0700

    right component rendering all icons volume bar needs work

---
+++ client/components/Connect.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Connect = function({currentState}){
+
+  if(currentState.deviceState === true)
+  {
+    return(
+
+      <div className="connect-device-list-container connect-device-list-container--is-visable">
+      <div className="connect-list">
+        <div className="connect-title" as="div" style={{color:'rgb(255,255,255)'}}>
+          <h3 className="connect-title-text" tabIndex="-1">Connect to a device</h3>
+          <a className="connect-title-help question-icon"></a>
+        </div>
+        <div className="connect-header">
+          <img className="connect-image" src="https://song-for-fake-spotify.s3-us-west-1.amazonaws.com/Screen+Shot+2020-06-22+at+10.43.15+PM.png"></img>
+        </div>
+        <div className="connect-info">
+          <p>Play and control spotify on all your devices.</p>
+          <p>Start spotify on another device and it will magically appear here.</p>
+          <a className="connect-device-button">Learn more</a>
+        </div>
+      </div>
+     </div>
+    )
+  }
+
+  if(currentState.deviceState===false){
+    return (
+      <div></div>
+    )
+  }
+}
+
+
+export default Connect;
+
+
+
--- client/components/Right.jsx
+++ client/components/Right.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
+import Connect from './Connect.jsx'
 
-const Right = function(props){
+const Right = function({currentState,handleQueueClick,handleDeviceClick,handleVolumeClick}){
   return(
-    <div className = 'controller_right'>right*</div>
+    <div className = 'controller_right'>
+      <div className = "controller-right-inner">
+        <div className="right-controls">
+          <div className="queue">
+            <div className={currentState.queueWrapper}>
+              <button className={currentState.queueIcon} onClick={(event)=>handleQueueClick(event)}></button>
+            </div>
+          </div>
+          <div className="extra-controls">
+            <span className="device-picker">
+              <button className="control-button device-icon" onClick={(event)=>handleDeviceClick(event)}></button>
+              <Connect currentState={currentState}/>
+            </span>
+          </div>
+          <div className="volume-bar" dir= 'ltr'>
+            <button className={currentState.volumeIcon} onClick={(event)=> handleVolumeClick(event)}></button>
+            <div className="progress-bar progress-bar--is-active">
+              <div className="middle-align progress-bar__bg">
+                <div className="progress-bar__fg_wrapper">
+                    <div className="progress-bar__fg" style={currentState.volumeRight}></div>
+                </div>
+                <button className=" middle-align progress-bar__slider" style={currentState.volumeLeft}></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
