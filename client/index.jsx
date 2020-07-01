bc14c9a0ab3dabc6bf3de4d6925bd9dd891e86b0
Ramos0921 <sfcc.eduardo.ramos@gmail.com>
2020-07-01 11:20:06 -0700

    fixed render name

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.jsx';
 //renders to html at app
-ReactDom.render(<App/>, document.getElementById('app'));
+ReactDom.render(<App/>, document.getElementById('control-bar-render'))
